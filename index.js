e41db51db15060e2547a040f873a0c11e0f529c4
Adam <ashaw315@gmail.com>
2021-11-29 13:34:26 -0500

    commit tree e0dd90180a5b895eef5654618d221b09e9977c0f

---
+++ index.js
@@ -0,0 +1,24 @@
+const numA = 2000
+const numB = 100
+
+const divide = function() {
+    return numA/numB;
+}
+divide();
+
+// const divide = (num1, num2) => num1/num2
+
+divide(2000, 100);
+
+const square = num => num * num
+
+square(10);
+
+let a = 3
+let b = 4
+
+const add = (a,b) => a + b
+add(a,b);
+
+
+
